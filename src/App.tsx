27298f2890c6edd349808da207d4ba468fe287a1
kyledavidd <davidyhuankyled@gmail.com>
2025-02-06 13:01:23 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="Name">
+      <h1>Yhuan Kyle David</h1>
+      <div className="Box">
+        <p>CPEITEL</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
